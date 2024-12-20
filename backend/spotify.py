c1e0f2f47d7d39a914a48fb7ae995543854bbaea
Julia Nguyen <juliatnguyen05@gmail.com>
2024-12-20 12:50:58 -0800

    feat: retrieve url leading to user authorization

--- backend/spotify.py
+++ backend/spotify.py
@@ -1,31 +1,37 @@
 from dotenv import load_dotenv
 from requests import post
-import os, base64, json, hashlib
+from urllib.parse import urlencode
+import os, base64, json, hashlib, webbrowser
 
 load_dotenv()
 
-client_id = os.getenv('CLIENT_ID')
-client_secret = os.getenv('CLIENT_SECRET')
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+REDIRECT_URI = 'http://EverWrapped/callback'
+SCOPE = 'user-read-private user-read-email'
+AUTHURL = 'https://accounts.spotify.com/authorize'
 
-def get_token() -> str:
-    """Get the generated token using the client id and secret"""
-    auth_string = f'{client_id}:{client_secret}'
-    auth_bytes = auth_string.encode('utf-8')
-    auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
+# def get_token() -> str:
+#     """Get the generated token using the client id and secret"""
+#     auth_string = f'{client_id}:{client_secret}'
+#     auth_bytes = auth_string.encode('utf-8')
+#     auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
 
-    url = "https://accounts.spotify.com/api/token"
-    headers = {
-        "Authorization": f'Basic {auth_base64}',
-        "Content-Type": 'application/x-www-form-urlencoded'
-    }
-    data = {"grant_type": 'client_credentials'}
-    result = post(url, headers = headers, data = data)
-    json_result = json.loads(result.content)
-    token = json_result['access_token']
+#     url = "https://accounts.spotify.com/api/token"
+#     headers = {
+#         "Authorization": f'Basic {auth_base64}',
+#         "Content-Type": 'application/x-www-form-urlencoded'
+#     }
+#     data = {"grant_type": 'client_credentials'}
+#     result = post(url, headers = headers, data = data)
+#     json_result = json.loads(result.content)
+#     token = json_result['access_token']
     
-    return token
+#     return token
 
-def _generate_random_str(length = 128) -> str:
+
+# Code Challenge generator
+def _generate_code_verifier(length = 128) -> str:
     """Generate a random str between 43 and 128 chars as a code verifier for PKCE standard"""
     random_bytes = os.urandom(length)
     code_verifier = base64.urlsafe_b64encode(random_bytes).rstrip(b'=')
@@ -42,4 +48,25 @@ def _hash_code(code: str) -> str:
 
 def get_code_challenge() -> str:
     """Generate code challenge from hashing a random string"""
-    return _hash_code(_generate_random_str())
+    return _hash_code(_generate_code_verifier())
+
+# Request user authorization and retrieve code
+def request_user_auth() -> str:
+    """Request user authorization to grant app perms"""
+    params = {
+        'response_type': 'code',
+        'client_id': CLIENT_ID,
+        'scope': SCOPE,
+        'code_challenge_method': 'S256',
+        'code_challenge': get_code_challenge(),
+        'redirect_uri': REDIRECT_URI,
+    }
+
+    auth_url_params = f'{AUTHURL}?{urlencode(params)}'
+    
+    return auth_url_params
+    # print(auth_url_params)
+    # print(f'Opening the following url in your browser: {auth_url_params}')
+    # webbrowser.open(auth_url_params)
+    # auth_code = input('Paste authorization code here: ')
+    # print(f'Code received: {auth_code}')
