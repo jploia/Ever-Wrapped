9a791875f097de9c8be8b941d73b1d89afc91b9f
Julia Nguyen <juliatnguyen05@gmail.com>
2024-12-27 18:29:21 -0800

    style: reorganize directories for clarity

--- src/backend/spotify.py
+++ src/backend/spotify.py
@@ -1,34 +1,12 @@
-from dotenv import load_dotenv
-from requests import post
+from flask import Flask, redirect, request, url_for
 from urllib.parse import urlencode
-import os, base64, json, hashlib, webbrowser
-
-load_dotenv()
+import os, base64, hashlib
 
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 REDIRECT_URI = 'http://EverWrapped/callback'
 SCOPE = 'user-read-private user-read-email'
 AUTHURL = 'https://accounts.spotify.com/authorize'
-
-# def get_token() -> str:
-#     """Get the generated token using the client id and secret"""
-#     auth_string = f'{client_id}:{client_secret}'
-#     auth_bytes = auth_string.encode('utf-8')
-#     auth_base64 = str(base64.b64encode(auth_bytes), 'utf-8')
-
-#     url = "https://accounts.spotify.com/api/token"
-#     headers = {
-#         "Authorization": f'Basic {auth_base64}',
-#         "Content-Type": 'application/x-www-form-urlencoded'
-#     }
-#     data = {"grant_type": 'client_credentials'}
-#     result = post(url, headers = headers, data = data)
-#     json_result = json.loads(result.content)
-#     token = json_result['access_token']
-    
-#     return token
-
 
 # Code Challenge generator
 def _generate_code_verifier(length = 128) -> str:
@@ -46,7 +24,7 @@ def _hash_code(code: str) -> str:
 
     return str(base64.b64encode(hash_bytes), 'utf-8')
 
-def get_code_challenge() -> str:
+def _get_code_challenge() -> str:
     """Generate code challenge from hashing a random string"""
     return _hash_code(_generate_code_verifier())
 
@@ -58,15 +36,23 @@ def request_user_auth() -> str:
         'client_id': CLIENT_ID,
         'scope': SCOPE,
         'code_challenge_method': 'S256',
-        'code_challenge': get_code_challenge(),
+        'code_challenge': _get_code_challenge(),
         'redirect_uri': REDIRECT_URI,
     }
 
     auth_url_params = f'{AUTHURL}?{urlencode(params)}'
     
     return auth_url_params
-    # print(auth_url_params)
-    # print(f'Opening the following url in your browser: {auth_url_params}')
-    # webbrowser.open(auth_url_params)
-    # auth_code = input('Paste authorization code here: ')
-    # print(f'Code received: {auth_code}')
+
+# def run_oauth_server() -> None:
+#     """Opens a browser for the user to authenticate their identity"""
+
+
+
+if __name__ == '__main__':
+    try:
+        print(request_user_auth())
+        
+    finally:
+        pass
+        
